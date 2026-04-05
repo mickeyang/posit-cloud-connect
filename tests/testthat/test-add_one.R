d0ee62384ef4fcaf71bba0a64c01d129fb1ff759
mickeyang <mickeyang@gmail.com>
2026-04-05 09:13:43 +0800

    add a wrong expect_equal() case for testing purpose

--- tests/testthat/test-add_one.R
+++ tests/testthat/test-add_one.R
@@ -1,5 +1,6 @@
 test_that("additive function is working fine,", {
   expect_equal(add_one(2), 3)
+  expect_equal(add_one(2), 5)
   expect_error(add_one(2), 0)
   expect_error(add_one("abc"))
   expect_error(add_one(NA_integer_))
