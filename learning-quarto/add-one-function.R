17ff8426aa28e751413b69a312e65f494034dd92
mickeyang <mickeyang@gmail.com>
2026-04-05 17:53:47 +0800

    refactor function

--- learning-quarto/add-one-function.R
+++ learning-quarto/add-one-function.R
@@ -1,1 +1,10 @@
-add_one <- function(x) x+1
+add_one <- function(x) {
+  if(is.na(x)) {
+    stop(simpleError("Cannot be a NULL value"))
+  }
+  tryCatch(
+    expr = return(x+1),
+    error = function(e) {
+      message(e)
+    })
+}
